ed907f26bac5b1eb9c9a11208131c97546a7b108
aanno <aanno@users.noreply.github.com>
2026-01-07 13:09:51 +0100

    added scripts/mcp-memory.sh

---
+++ scripts/mcp-memory.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://github.com/doobidoo/mcp-memory-service.git
+# python
+# must run _outside_ dev container
+
+pnpm dlx supergateway --stdio 'uv --directory /stratis/home/tpasch/dev/scm/github/mcp-memory-service run memory' --port 3012 --baseUrl http://localhost:3012 --ssePath /sse --messagePath /message
